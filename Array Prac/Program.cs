06153886515216ca673544d03ade75cb8087f7ab
Steven Castano <s210717@students.aie.edu.au>
2021-05-18 16:04:48 +1000

    prac

---
+++ Array Prac/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Array_Prac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "this", "is", "an", "array", "full", "of", "needle", "random", "stuff" };
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+
+            Console.WriteLine(FindNeedle(array));
+        }
+
+        static int FindNeedle(string[] words)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] == "needle")           
+                return i;
+            }
+
+            return -1;
+        }
+    }
+}
+
---
+++ array Prac int/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace array_Prac_int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myInts = new int[10] { 1200, 19, 27, 105, 44, 555, 63, 52, 87, 99 };
+
+            for (int j = 0; j < 0 + 1; j++)
+            {
+                for (int i = 9; i > -1; i--)
+                {
+                    Console.WriteLine(myInts[i]);
+                }
+            } 
+        }
+    }
+}
